f9f0bf1be2dd8594c8f18f5454fe82479950b2a3
venky <venky@ADMINRG-CCB3U58>
2017-12-29 20:13:08 -0800

    FROUNT END PRACTICE

---
+++ myFrontend/src/main/java/com/niit/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.niit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+class UserController {
+	@RequestMapping(value="/index",method=RequestMethod.GET)
+	public String signin()
+	{
+		System.out.println("inside controller");
+		return "index";
+	}
+
+}
