a7f98390c76973954af01896d681f7568cfcd1c8
venky <venky@ADMINRG-CCB3U58>
2018-01-07 23:22:15 +0530

    jan 6

--- Myprojectbackend/src/main/java/com/testingtable/dao/cartdao.java
+++ Myprojectbackend/src/main/java/com/testingtable/dao/cartdao.java
@@ -3,7 +3,6 @@ package com.testingtable.dao;
 import java.util.List;
 
 import com.testingtable.model.Category;
-import com.testingtable.model.cart;
 
 public interface cartdao {
 	public List<cart> getAllcarts();
